50b637a3c1accd3ea7426079b742007b203c8433
jijup <jijupnair2000@gmail.com>
2018-12-25 14:31:35 -0400

    Changes to IncrementalAlgo.cpp

--- IncrementalAlgo.cpp
+++ IncrementalAlgo.cpp
@@ -11,7 +11,8 @@
 // includes for defining the Voronoi diagram adaptor
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
-#include <CGAL/Voronoi_diagram_2.h>
+#include "Voronoi_diagram_2.h"
+
 #include <CGAL/Delaunay_triangulation_adaptation_traits_2.h>
 #include <CGAL/Delaunay_triangulation_adaptation_policies_2.h>
 
